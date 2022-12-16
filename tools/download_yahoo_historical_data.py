f330305729b202fb471d93541de26ff8932b7255
George Vine <vinegh4@gmail.com>
2022-12-16 15:12:01 -0800

    initial commit

---
+++ tools/download_yahoo_historical_data.py
@@ -0,0 +1,4 @@
+#Link for downloading max historical data on JPM going back from 12/16/2022
+#https://query1.finance.yahoo.com/v7/finance/download/JPM?period1=322099200&period2=1671148800&interval=1d&events=history&includeAdjustedClose=true
+
+#TODO get the above data for a list of tickers by argument and write to csv
